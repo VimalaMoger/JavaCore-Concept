041d4daaa102687de301cdef5b82f642f15a8e6c
Vimala Moger <vimalamoger@gmail.com>
2024-09-25 09:56:53 -0500

    Resolve merge conflict

--- src/java9/AbstractMethodImpl.java
+++ src/java9/AbstractMethodImpl.java
@@ -1,13 +1,13 @@
 package java9;
 
-public class abstractMethodImpl implements privateMethodImpl {
+public class AbstractMethodImpl implements PrivateMethodImpl {
         @Override
         public void abstractMethodExample() {
             System.out.println
                     ("Inside the implementation of an abstract method");
         }
         public static void main(String[] args) {
-            privateMethodImpl test = new abstractMethodImpl();
+            PrivateMethodImpl test = new AbstractMethodImpl();
             test.defaultMethodExample();
             test.abstractMethodExample();
         }
---
+++ src/java9/PrivateMethodImpl.java
@@ -0,0 +1,22 @@
+package java9;
+
+public interface PrivateMethodImpl {
+
+    public abstract void abstractMethodExample();
+
+    public
+    default void defaultMethodExample() {
+        privateMethodExample();
+        privateStaticMethodExample();
+        System.out.println("Inside a default method");
+    }
+
+    private void privateMethodExample() {
+        System.out.println("Inside a private non-static method");
+    }
+
+    private static void privateStaticMethodExample() {
+        System.out.println("Inside a private static method");
+    }
+
+}
