a0da5d1b6edc702e8d1fe76750f4666dcd166bce
VimalaMoger <166641453+VimalaMoger@users.noreply.github.com>
2024-09-26 15:02:37 -0500

    Delete src/jdk15 directory
    duplicate one

--- src/jdk15/Tool.java
+++
@@ -1,7 +0,0 @@
-package jdk15;
-
-public abstract class Tool { //use either abstact or interface to remove common code in other classes
-    public abstract void mouseDown();
-    public abstract void mouseUp();
-
-}
